544fac8ad3680189d6581ee0fe5089df25fa2be9
franknfjr <frankdonascimentojr@gmail.com>
2017-07-22 17:01:19 -0300

    updates BubbleSSortOptimized1

--- BubbleSortOptimized1.java
+++ BubbleSortOptimized1.java
@@ -4,24 +4,27 @@ public class BubbleSortOptimized1{
   public static void main(String[] args) {
 
     int x[] = new int [5];
-    int j,i,aux;
+    int j,i,aux,cont=0;
     Scanner entrada = new Scanner (System.in);
     for (i=0;i<=4 ;i++ ) {
       System.out.println("Digite o "+(i+1) +"o numero: " );
       x[i] = entrada.nextInt();
     }
     for (j=1;j<=4 ;j++ ) {
+      cont = cont +1;
       for (i=4;i>=j ;i-- ) {
           if (x[i]<= x[i-1]) {
             aux = x[i];
             x[i] = x[i-1];
             x[i-1] = aux;
-            System.out.print(x[i] +" <= "+ x[i-1] +" is ");
+            System.out.print(cont +"º  "+ x[i] +" <= "+ x[i-1] +" is ");
             System.out.println(x[i]<= x[i-1]);
+            cont++;
           }
           else{
-            System.out.print(x[i] +" > "+ x[i-1] +" is ");
+            System.out.print(cont +"º  "+ x[i] +" > "+ x[i-1] +" is ");
             System.out.println(x[i] > x[i-1]);
+            cont++;
           }
       }
     }
