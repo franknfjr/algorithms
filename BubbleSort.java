084c99763034aa903c76b126dafbb08701e0f057
franknfjr <frankdonascimentojr@gmail.com>
2017-07-22 16:50:17 -0300

    updates BubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,24 +4,27 @@ public class BubbleSort{
   public static void main(String[] args) {
 
     int x[] = new int [5];
-    int n, i, aux;
+    int n, i, aux, cont=0;
     Scanner entrada = new Scanner (System.in);
     for (i=0; i<=4 ; i++ ) {
       System.out.println("Digite o " + (i + 1) +" o número.");
       x[i] = entrada.nextInt();
     }
       for (n =0 ;n<=4 ;n++ ) {
+        cont = cont +1;
           for (i=0;i<=3 ;i++ ) {
               if (x[i] > x[i+1]) {
                 aux = x[i];
                 x[i] =  x[i + 1];
                 x[i + 1] = aux;
-                System.out.println(x[i] +" > "+ x[i+1]);
+                System.out.print(cont +"º  "+ x[i] +" > "+ x[i+1] +" is ");
                 System.out.println(x[i] > x[i+1]);
+                cont++;
               }
               else{
-                System.out.println(x[i] +" < "+ x[i+1]);
+                System.out.print(cont +"º  "+ x[i] +" < "+ x[i+1] +" is ");
                 System.out.println(x[i] < x[i+1]);
+                cont++;
               }
           }
       }
