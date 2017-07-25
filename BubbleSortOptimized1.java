cf73e8a57cb5f5604844bf81ff799740e7f36f97
franknfjr <frankdonascimentojr@gmail.com>
2017-07-24 21:52:11 -0300

    fix updates BubbleSortOptimized1

--- BubbleSortOptimized1.java
+++ BubbleSortOptimized1.java
@@ -1,17 +1,17 @@
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class BubbleSortOptimized1{
   public static void main(String[] args) {
 
     int x[] = new int [5];
-    int j,i,aux,cont=0;
+    int j,i,aux,cont=1;
     Scanner entrada = new Scanner (System.in);
-    for (i=0;i<=4 ;i++ ) {
-      System.out.println("Digite o "+(i+1) +"o numero: " );
-      x[i] = entrada.nextInt();
-    }
+      for (i=0;i<=4 ;i++ ) {
+        System.out.println("Digite o "+(i+1) +"o numero: " );
+        x[i] = entrada.nextInt();
+      }
     for (j=1;j<=4 ;j++ ) {
-      cont = cont +1;
       for (i=4;i>=j ;i-- ) {
           if (x[i]<= x[i-1]) {
             aux = x[i];
@@ -19,14 +19,16 @@ public class BubbleSortOptimized1{
             x[i-1] = aux;
             System.out.print(cont +"º  "+ x[i] +" <= "+ x[i-1] +" is ");
             System.out.println(x[i]<= x[i-1]);
-            cont++;
+
           }
           else{
             System.out.print(cont +"º  "+ x[i] +" > "+ x[i-1] +" is ");
             System.out.println(x[i] > x[i-1]);
-            cont++;
+
           }
       }
+      System.out.println((j) + " laço " +  ": " + Arrays.toString(x) +"\n");
+      cont++;
     }
 
     for (i=0;i<=4 ;i++ ) {
