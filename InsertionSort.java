9f46558d7a741b9d5c1d24d8bfa909adb52d9bb1
Frank <frankdonascimentojr@gmail.com>
2017-08-15 22:45:15 -0300

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,7 +4,7 @@ public class InsertionSort{
   public static void main(String[] args) {
 
     int x[] = new int [5];
-    int i,j,eleito;
+    int i,j,eleito,cont=1;
     Scanner entrada = new Scanner (System.in);
       for (i=0;i<=4 ;i++ ) {
         System.out.println("Digite o "+(i+1) +"o numero: " );
@@ -15,11 +15,19 @@ public class InsertionSort{
         eleito= x[i];
         j=i-1;
         while( j>= 0 && x[j] > eleito){
-          x[j+1] = x[j];
-          j=j-1;
-
+          if(x[j] > eleito){
+		            System.out.print(cont +"º  "+ x[j] +" > "+ eleito +" is ");
+                System.out.println(x[j] > eleito);		
+		            }
+		            else{
+		            System.out.print(cont +"º  "+ x[j] +" < "+ eleito +" is ");
+                System.out.println(x[j] < eleito);
+		 }
+		 x[j+1] = x[j];
+     j=j-1;
         }
         x[j+1] = eleito;
+        cont++;
       }
       for (i=0;i<=4 ; i++ ) {
         System.out.println((i+1)+"o numero: " + x[i]);
